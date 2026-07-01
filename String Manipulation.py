34a06852c8bb32caf382d7119a6523145e0dd6bd
Sattanathan2 <sattanathan2025@gmail.com>
2026-07-01 21:40:55 +0530

    Day 01: Python basics and Band Name Generator
    day-01/String Manipulation.py
    day-01/Variable Naming.py
    day-01/Variables.py
    day-01/Band Name Generator Project.py
    day-01/Inputs.py
    day-01/Printing.py

---
+++ String Manipulation.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+print("Hello world!\nHello World!\nHello world!")
+
+
+#PAUSE 2. Add a space between the strings
+
+print("hello "+"Anglea")
+
+
---
+++ Variables.py
@@ -0,0 +1,11 @@
+You= "tony"
+print(You)
+
+You="sattanathan"
+print(You)
+
+
+username = input("What is your name?")
+length = len(username)
+print(length)
+

a3b386b1e6bff3b92176c0e0cb2cf4065f3c2ba4
Sattanathan2 <sattanathan2025@gmail.com>
2026-07-02 21:46:03 +0530

    Day 2: Add Python exercises and Tip Calculator
    Completed Day 2 of the 100 Days of Code Python Bootcamp.
    
    Files included:
    - Data Types
    - Mathematical Operations
    - Number Manipulation
    - Type Error, Checking & Conversion
    - Tip Calculator Project

---
+++ Day-02/Data Types.py
@@ -0,0 +1,16 @@
+print("hello "[0])
+
+#string
+print("123" + "345")
+
+#integers
+print( 123 + 456)
+# Large Integers
+print(1234_44455_7777)
+#Floats
+print(1.00)
+#booleans
+print(True)
+print(False)
+len("Hello")
+
---
+++ Day-02/Type Error,Checking and Conversion.py
@@ -0,0 +1,38 @@
+# TypeError
+# len(123)
+
+# No TypeError
+len("Hello")
+
+# Type Checking
+print(type("abc"))
+print(type(123))
+print(type(3.14))
+print(type(True))
+
+# Type Conversion
+str()
+int()
+float()
+bool()
+name_of_the_user = input("Enter your name")
+length_of_name = len(name_of_the_user)
+print(type("Number of letters in your name: "))  # str
+print(type(length_of_name))  # int
+print("Number of letters in your name: " + str(length_of_name))
+print("hello "[0])
+
+#string
+print("123" + "345")
+
+#integers
+print( 123 + 456)
+# Large Integers
+print(1234_44455_7777)
+#Floats
+print(1.00)
+#booleans
+print(True)
+print(False)
+len("Hello")
+
